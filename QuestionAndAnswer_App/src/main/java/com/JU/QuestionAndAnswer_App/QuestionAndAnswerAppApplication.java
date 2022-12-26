129c1ca0715767cc52e0a3b7cb3034da76a13f1d
Jathusan Uthayakumaran <juthayakumara21@hotmail.co.uk>
2022-12-26 15:02:47 +0000

    first commit

---
+++ QuestionAndAnswer_App/src/main/java/com/JU/QuestionAndAnswer_App/QuestionAndAnswerAppApplication.java
@@ -0,0 +1,13 @@
+package com.JU.QuestionAndAnswer_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionAndAnswerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionAndAnswerAppApplication.class, args);
+	}
+
+}
---
+++ QuestionAndAnswer_App/src/test/java/com/JU/QuestionAndAnswer_App/QuestionAndAnswerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JU.QuestionAndAnswer_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionAndAnswerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
